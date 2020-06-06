7d7348a7adb68727bb91545041ff2279d7e782be
Brandon Macleod <BrandonMacleod21@gmail.com>
2020-06-06 01:30:14 +0000

    TODO program.

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
-// Arrays to keep track of each task's state
+function newTask(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false,
+    
+    logState: function() {
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    },
+
+    markCompleted: function() {
+      this.complete = true;
+    }
+  };
+  return task;
+}
+
+// DRIVER CODE BELOW
+
+const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
+const task2 = newTask("Do Laundry", "😨");
+const tasks = [task1, task2];
+
+task1.logState();
+task1.markCompleted();
+task1.logState();
+
+// for now, let's just make sure we see our tasks
+console.log(tasks);
+
+
+/*// Arrays to keep track of each task's state
 const taskTitles = [];
 const taskComplete = [];
 
@@ -28,4 +59,4 @@ newTask("Do Laundry"); // task 1
 
 logTaskState(0); // Clean Cat Litter has not been completed
 completeTask(0);
-logTaskState(0); // Clean Cat Litter has been completed
+logTaskState(0); // Clean Cat Litter has been completed*/
